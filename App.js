b7c189f91d3183df44b7257afa9221acc55cb060
Julia Stamborovski <juliashizuko66@gmail.com>
2023-06-01 00:44:32 -0300

    init projeto e componentes de função

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import React from 'react';
+import Basket from './src/screens/Basket'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>hi julia! 😶‍🌫️ </Text>
+      <Basket />
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/screens/Basket.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+export default function Basket() {
+    return (
+        <Text>🧺 Basket Component 🧺</Text>
+    );
+}
+
