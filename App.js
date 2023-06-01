2b96cb566584be04850ef47d64b31bd37694fa0d
Julia Stamborovski <juliashizuko66@gmail.com>
2023-06-01 13:04:31 -0300

    imagens e estilos

--- App.js
+++ App.js
@@ -1,22 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import { StatusBar, SafeAreaView, StyleSheet } from 'react-native';
 import Basket from './src/screens/Basket'
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>hi julia! 😶‍🌫️ </Text>
+    <SafeAreaView>
+      <StatusBar />
       <Basket />
-      <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/screens/Basket.js
+++ src/screens/Basket.js
@@ -1,9 +1,34 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image, StyleSheet, Dimensions, Text, SafeAreaView} from 'react-native';
+import topo from '../../assets/topo.png'
+
+const width = Dimensions.get('screen').width;
 
 export default function Basket() {
     return (
-        <Text>🧺 Basket Component 🧺</Text>
+        <SafeAreaView>
+        <Image source={topo} style={styles.topo} />
+        <Text style={styles.title}>Detalhes da Cesta</Text>
+        </SafeAreaView>
     );
 }
 
+const styles = StyleSheet.create(
+    {
+        topo: {
+            width: '100%',
+            height: 578 / 768 * width,
+        
+        },
+        title: {
+            width: '100%',
+            position: 'absolute',
+            textAlign: 'center',
+            fontSize: '16px',
+            lineHeight: 26,
+            color: '#fff',
+            fontWeight: 'bold',
+            padding: 16,
+        }
+    }
+);
